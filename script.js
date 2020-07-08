744e718b94280c803c91769635298983c1365f87
Kira <kira@dogmail.com>
2020-07-07 22:02:43 -0400

    working draft.v4

--- script.js
+++ script.js
@@ -14,11 +14,11 @@ Quiz.prototype.isEnded = function() {
 }
 
 Quiz.prototype.guess = function (answer) {
-    this.questionsIndex++;
-
     if(this.getQuestionIndex ().correctAnswer (answer)) {
         this.score++;
     }
+
+    this.questionsIndex++;
 }
 
 // Questions Fxn:
@@ -47,15 +47,33 @@ function populate () {
         for(var i=0; i < choices.length; i++) {
            var element = document.getElementById("choice" + i); 
            element.innerHTML = choices[i];
+           guess("btn" + i, choices[i]); 
         }
+
+        showProgress();
     }
 };
+
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function () {
+        quiz.guess(guess);
+        populate();
+    }
+};
+
+function showProgress() {
+    var currentQuestionNumber = quiz.questionsIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+}
 
 function showScores() {
     var gameOverHtml = "<h1>Result</h1>";
     gameOverHtml += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
     var element = document.getElementById("quiz");
-}
+    element.innerHTML = gameOverHtml;
+};
 
     //Logic from Question(text, choices, answer)
 var questions = [
