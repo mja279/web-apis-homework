a08f56d649dd5687659d653923182742991b7e50
Kira <kira@dogmail.com>
2020-07-08 11:48:05 -0400

    working draft.v7

---
+++ timer.js
@@ -0,0 +1,28 @@
+var timeEl = document.querySelector(".time");
+var mainEl = document.getElementById("main");
+
+var secondsLeft = 3;
+
+function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left";
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      sendMessage();
+    }
+
+  }, 1000);
+}
+
+function sendMessage() {
+  timeEl.textContent = "Please select your answer immediately";
+
+  if(secondsLeft === 0) {
+    alert("TIME IS UP!");
+  }
+
+}
+
+setTime();
